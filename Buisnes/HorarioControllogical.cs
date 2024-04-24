986260778aab69acd5b52bd3334627ff4f551d38
mario1525 <mbeltran4892@gmail.com>
2024-04-23 22:53:03 -0500

    add horarioServices

---
+++ Buisnes/HorarioControllogical.cs
@@ -0,0 +1,72 @@
+﻿using Data;
+using Entity;
+using System.Data;
+using System.Threading.Tasks;
+
+namespace Buisnes
+{
+    public class HorarioControllogical
+    {
+        private readonly DAOHorario _daoHorario;
+
+        public HorarioControllogical(DAOHorario daoHorario)
+        {
+            _daoHorario = daoHorario;
+
+        }
+
+        public async Task<Guid> guardarHorario(Horario horario)
+        {
+            
+            return await  _daoHorario.UpdateDocument(horario);
+
+        }
+
+        public Horario mostrarDocumento(string id)
+        {
+            // Mapear DataTable a lista de archivos
+            Task<DataTable> taskDataTable = _daoHorario.descarDocument(id);
+            DataTable user = taskDataTable.Result;
+            //Console.WriteLine(user.Rows.Count);
+            Horario archivos = MapDataTableArchivoList(user);
+            return archivos;
+        }
+
+        public string buscarHorario(string salon)
+        {
+            Task<DataTable> taskDataTable =  _daoHorario.GetSalon(salon);
+            DataTable archivo = taskDataTable.Result;
+            string id = archivo.ToString();
+
+            return id;
+        }
+
+        public string eliminarHorario(string id) 
+        {
+            Task<DataTable> taskDataTable = _daoHorario.deleteHorario(id);
+            DataTable elm = taskDataTable.Result;
+            string data = elm.ToString();
+            return data;
+        }
+
+        private static Horario MapDataTableArchivoList(DataTable dataTable)
+        {
+           Horario ArchivosList = new Horario();
+
+            foreach (DataRow row in dataTable.Rows)
+            {
+                Horario archivo = new Horario
+                {
+                    Nombre_Archivo = row["Nombre_Archivo"].ToString(),
+                    Extension = row["Extension"].ToString(),
+                    Formato = row["Formato"].ToString(),
+                    Archivo = ((byte[])row["Archivos"]).ToArray(),
+                    Tamanio = Convert.ToDouble(row["Tamanio"]),
+                    // Asigna otras propiedades según tu DataTable y clase Usuarios
+                };
+                ArchivosList= archivo;
+            }
+            return ArchivosList;
+        }
+    }
+}
---
+++ Services/HorarioServices.cs
@@ -0,0 +1,85 @@
+﻿using Buisnes;
+using Entity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Services
+{
+    [ApiController]
+    [Route("api/descargas")]
+    public class DescargaController : ControllerBase
+    {
+
+        private readonly HorarioControllogical _doConLogical;
+
+        public DescargaController(HorarioControllogical doConLogical)
+        {
+            _doConLogical = doConLogical;
+        }
+
+
+        [HttpPost]
+        public mensajes UploadFile(IFormFile[] archivos)
+        {
+            if (archivos.Length == 0)
+            {
+                mensajes mensaje = new mensajes();
+                mensaje.mensaje = "no se paso ningun archivo";
+                return mensaje;
+            }
+            else
+            {
+                for (int i = 0; i < archivos.Length; i++)
+                {
+                    Horario horario = new Horario();
+
+                    horario.Nombre_Archivo = Path.GetFileNameWithoutExtension(archivos[i].FileName);
+                    horario.Extension = Path.GetExtension(archivos[i].FileName);
+                    horario.Formato = archivos[i].ContentType;
+
+                    long longitudArchivo = archivos[i].Length;
+
+                    // Convierte el tamaño a megabytes
+                    double tamanio = longitudArchivo / 1000000.0;
+                    horario.Tamanio = Math.Round(tamanio, 2);
+                    
+
+                    // Lee el archivo en un array de bytes
+                    using (MemoryStream ms = new MemoryStream())
+                    {
+                        archivos[i].CopyTo(ms);
+                        horario.Archivo = ms.ToArray();
+                    }
+
+
+
+                    _doConLogical.guardarHorario(horario);
+                }
+
+
+
+                mensajes mensaje1 = new mensajes();
+                mensaje1.mensaje = "archivo guardado";
+                return mensaje1;
+            }
+
+        }
+
+
+
+
+        [HttpGet("{id}")]
+        public IActionResult DownloadImage(string id)
+        {
+            // Obtener los datos del horario desde la base de datos
+            Horario horario = _doConLogical.mostrarDocumento(id);
+
+            if (horario == null)
+            {
+                return NotFound(); // Devolver un 404 si no se encuentra el horario
+            }
+
+            // Devolver el archivo como un FileContentResult
+            return File(horario.Archivo, "application/pdf", horario.Nombre_Archivo);
+        }
+    }
+}
